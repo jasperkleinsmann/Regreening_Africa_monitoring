370878aa422cd67c71c25044a700010aeba550a6
Jasper Kleinsmann <jasper.kleinsmann@wur.nl>
2022-12-05 10:33:33 +0300

    gpu commit

--- A8-ts_interpret.R
+++ A8-ts_interpret.R
@@ -19,7 +19,6 @@ plots <- st_read('output/plot_data/all_countries/Countries_plots_final.GeoJSON')
 l8_fc_plt_dt <- l8_fc_plt[,c('.mean', 'yearmonth', 'plotID', 'upper_95', 'lower_95')]
 l8_fc_plt_dt$yearmon <- as.Date(ISOdate(year(l8_fc_plt_dt$yearmonth), month(l8_fc_plt_dt$yearmonth), 15))
 
-
 # Merge actual ndvi with the forecast
 l8_fc_plt_dt <- merge(l8_fc_plt_dt, l8_ts_plt[,c('ndvi_int', 'plotID', 'yearmon')], 
                    by=c('yearmon', 'plotID'), all.x=T)
@@ -39,8 +38,13 @@ l8_green <- l8_fc_plt_dt %>%
 #Merge
 plots <- merge(plots, l8_green, by='plotID', all.x=T, all.y=F)
 plots$l8_green[is.na(plots$l8_green)] <- 0
+fwrite(l8_green, 'output/models/Countries_l8_green.csv')
 
-
+# Add the residuals per plot to the plots df
+l8_armax_plt %>% 
+  head(100) %>% 
+  residuals() %>% 
+  select(.resid)
 
 #### Save the new plots dataset including greening information
 st_write(plots, dsn='output/plot_data/Countries_plots_green.GeoJSON', driver='GeoJSON')
@@ -57,28 +61,28 @@ cnt_green <- plots_dt[,.(l8_green=sum(regreening,na.rm=T),
                          l8_ha=sum(Hectare),
                          l8_ha_green=sum(Hectare[regreening==1],na.rm=T),
                          l8_ha_perc=sum(Hectare[regreening==1],na.rm=T)/sum(Hectare)),
-                      by=list(country)]
+                      by=list(county)]
 cnt_green
 
 
 
 
 
-type_green <- plots_dt[,.(l8_green=sum(l8_green),
-                         number_sites=length(l8_green),
-                         perc=sum(l8_green)/length(l8_green),
-                         l8_ha=sum(Hectare),
-                         l8_ha_green=sum(Hectare[l8_green==1]),
-                         l8_ha_perc=sum(Hectare[l8_green==1])/sum(Hectare)),
+type_green <- plots_dt[,.(l8_green=sum(regreening,na.rm=T),
+                          number_sites=length(regreening),
+                          perc=sum(regreening,na.rm=T)/length(regreening),
+                          l8_ha=sum(Hectare),
+                          l8_ha_green=sum(Hectare[regreening==1],na.rm=T),
+                          l8_ha_perc=sum(Hectare[regreening==1],na.rm=T)/sum(Hectare)),
                       by=list(type)]
 type_green
 
 # Exclude plots where no model was fit
-cnt_green_fit <- plots_dt[,.(l8_green=sum(l8_green),
+cnt_green_fit <- plots_dt[,.(l8_green=sum(l8_green,na.rm=T),
                          number_sites=length(l8_green[fit==1]),
-                         perc=sum(l8_green)/length(l8_green[fit==1]),
+                         perc=sum(l8_green,na.rm=T)/length(l8_green[fit==1]),
                          l8_ha=sum(Hectare[fit==1], na.rm=T),
-                         l8_ha_green=sum(Hectare[l8_green==1]),
+                         l8_ha_green=sum(Hectare[l8_green==1],na.rm=T),
                          l8_ha_perc=sum(Hectare[fit==1], na.rm=T)/sum(Hectare)),
                       by=list(county)]
 cnt_green_fit
